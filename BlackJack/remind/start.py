bb684b71161e3c1596c1a74d57a9ca2e9ae16d04
thalyta.cf <thalytacfischer@gmail.com>
2020-03-13 12:04:52 -0300

    update blackjack 2

---
+++ BlackJack/remind/start.py
@@ -0,0 +1,18 @@
+#importar o game e instanciar varios gamers
+from .main import
+from .player import Player
+
+
+
+
+class Start:
+
+    def __init__(self, game:,player:Player):
+        self.game = game
+        self.player = player
+
+
+
+
+
+Start(xx,'Thalyta')
